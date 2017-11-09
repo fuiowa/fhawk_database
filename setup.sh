7fd8029637c4d72aaa6a290b7050972bce7f078b
notification.uiowa <notification.uiowa@gmail.com>
2017-11-08 21:04:18 -0600

    new

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ mkdir -p ~/fhawk/fhawk_database
 cp * ~/fhawk/fhawk_database
 echo "alias fhawk_data=\"cd; cd fhawk/fhawk_database/; python2.7 fhawk_data\"" >> ~/.bash_profile
 alias fhawk_data="cd; cd fhawk/fhawk_database; python2.7 fhawk_data"
+rm ~/fhawk/fhawk_database/setup.sh
 rm -f -r ../fhawk_database
-cd
+
