64bf972bdb1c0f0893469e4d2a52639e9baf14d2
notification.uiowa <notification.uiowa@gmail.com>
2017-11-08 20:32:12 -0600

    fixed a bug

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo easy_install-2.7 pycrypto
-mkdir -p ~/fhawk_database
-cp * ~/fhawk_database
-echo "alias fhawk_data=\"cd; cd fhawk_database; python2.7 fhawk_data\"" >> ~/.bash_profile
+mkdir -p ~/fhawk/fhawk_database
+cp * ~/fhawk/fhawk_database
+echo "alias fhawk_data=\"cd; cd fhawk/fhawk_database/; python2.7 fhawk_data\"" >> ~/.bash_profile
 source ~/.bash_profile
 rm -f -r ../fhawk_database
