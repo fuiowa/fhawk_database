0a3d76f32bac3bd6caaf48d445634d83dd7e9e59
notification.uiowa <notification.uiowa@gmail.com>
2017-11-08 20:51:17 -0600

    fixed the alias

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sudo easy_install-2.7 pycrypto
 mkdir -p ~/fhawk/fhawk_database
 cp * ~/fhawk/fhawk_database
 echo "alias fhawk_data=\"cd; cd fhawk/fhawk_database/; python2.7 fhawk_data\"" >> ~/.bash_profile
-source ~/.bash_profile
+alias fhawk_data="cd; cd fhawk/fhawk_database; python2.7 fhawk_data"
 rm -f -r ../fhawk_database
